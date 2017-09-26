63d9e9246080a6e68b2cb42e44c1c6d33be6bcbb
John Alford <jalford@Johns-MacBook-Pro.local>
2017-09-26 12:03:47 -0400

    Changed default dielectric.cpp (Luneberg lens)

--- src/dielectric.cpp
+++ src/dielectric.cpp
@@ -1,35 +1,29 @@
 /**
- *  dielectric.cpp: Dielectric function for a single uniform cylinder
- *                  Long axis (height axis) is along the z axis
- *                  
- *                  
- *
+ *  dielectric.cpp: Dielectric function of the spherical Luneburg lens
+ *                  eps ~ r^{2}
  */
 #include<dielectric.hpp>
-std::complex<REAL_TYPE> 
-dielectric(const REAL_TYPE &zc, const REAL_TYPE &phi,const REAL_TYPE &theta,const REAL_TYPE &r)
+std::complex<REAL_TYPE>  
+dielectric(const REAL_TYPE& zc,const REAL_TYPE &phi,const REAL_TYPE &theta,const REAL_TYPE &r)
 {
+  const REAL_TYPE ZERO= 0.000000000000000000000L;
+  const REAL_TYPE ONE=  1.000000000000000000000L;
+  const REAL_TYPE TWO=  2.000000000000000000000L;
+  const REAL_TYPE  P5=  0.500000000000000000000L;
+  const REAL_TYPE FOUR= 4.000000000000000000000L;
 
-  // Constants
-  const REAL_TYPE ZERO = (REAL_TYPE)0.000000000000000000000000L;
-  const REAL_TYPE P5   = (REAL_TYPE)0.5000000000000000000000000L;
-  const REAL_TYPE ONE=  (REAL_TYPE) 1.000000000000000000000000L;
-  const REAL_TYPE TWO=  (REAL_TYPE) 2.000000000000000000000000L;
   
   
-  //const REAL_TYPE zc =  ZERO;  // shift along z
-  const REAL_TYPE radius =3.978873577297384; // Radius of cylinder
-  const REAL_TYPE height =TWO*radius; // half height
-  const std::complex<REAL_TYPE> dielectricConstant= std::complex<REAL_TYPE>(2.340836L,0.02448L);
-
-  // REAL_TYPE x = r*sin(theta)*cos(phi);
-  // REAL_TYPE y = r*sin(theta)*sin(phi);
+  REAL_TYPE x = r*sin(theta)*cos(phi);
+  REAL_TYPE y = r*sin(theta)*sin(phi);
   REAL_TYPE z = r*cos(theta);
-  REAL_TYPE dz = z-zc;
-  //REAL_TYPE dr = sqrt(x*x + y*y);
-  REAL_TYPE dr = r*std::abs(sin(theta));
+
+  REAL_TYPE dz = z - zc;
+  REAL_TYPE d = sqrt(x*x + y*y + dz*dz);
   
-  if((dr < radius) && (dz> (-height)) && (dz < height) ) return dielectricConstant;
+  if(d < P5) {
+    return std::complex<REAL_TYPE>((TWO - FOUR*d*d),ZERO);
+  }
   return std::complex<REAL_TYPE>(ONE,ZERO);
 
 }
